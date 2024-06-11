dd933da25ab169b457d7b145b12807fe707d516c
kalimandz <59579552+kalimandz@users.noreply.github.com>
2024-06-11 17:20:34 -0600

    Create listas.py

---
+++ listas.py
@@ -0,0 +1,25 @@
+# Crear una lista
+lista_pokemons = ["moltres", "zapdos", "articuno"]
+
+print(lista_pokemons)
+
+# Accediendo a Elementos dentro de una lista
+print(lista_pokemons[0])
+
+# Usando metodos de cadena
+print(f"{lista_pokemons[0].upper()}")
+
+# Posiciones de Indice
+print(lista_pokemons[0])
+print(lista_pokemons[1])
+print(lista_pokemons[2])
+
+# Indices Negativos
+print(lista_pokemons[-1])
+print(lista_pokemons[-2])
+print(lista_pokemons[-3])
+
+# Usando valores individuales
+mensaje = f"{lista_pokemons[0].upper()} es un pokemon legendario"
+
+print(mensaje)
